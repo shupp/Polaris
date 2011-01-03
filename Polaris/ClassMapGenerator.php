52c97722740e2498c25f6c491d44351e523286eb
Bill Shupp <bshupp@empowercampaigns.com>
2011-01-03 10:40:54 -0800

    Added ability to pass in 'find' paths to map constructor.  Removed trailing whitespace

--- Polaris/ClassMapGenerator.php
+++ Polaris/ClassMapGenerator.php
@@ -3,10 +3,10 @@
 /**
  * A tool for mapping class and interface names to files.  For use with
  * autoloading
- * 
+ *
  * @category  Zend
  * @package   Polaris
- * @author    Bill Shupp <hostmaster@shupp.org> 
+ * @author    Bill Shupp <hostmaster@shupp.org>
  * @copyright 2010 Bill Shupp
  * @license   http://www.opensource.org/licenses/bsd-license.php FreeBSD
  * @link      http://github.com/shupp/polaris
@@ -15,34 +15,39 @@ class Polaris_ClassMapGenerator
 {
     /**
      * The find paths to use with the find command when used.  Defaults to "."
-     * 
+     *
      * @var string
      */
     public $findPaths = ".";
 
     /**
      * Storage of the complete file list to parse
-     * 
+     *
      * @var string
      */
     public $fileList  = null;
 
     /**
      * Map of class names to filenames.
-     * 
+     *
      * @var array
      */
     public $classMap = array();
 
     /**
      * Sets $fileList, indicating which files to parse
-     * 
-     * @param string $file Optional filename from which to get the file list
-     * 
+     *
+     * @param string $file  Optional filename from which to get the file list
+     * @param string $paths Optional string of paths to use with the "find" command
+     *
      * @return void
      */
-    public function __construct($file = null)
+    public function __construct($file = null, $paths = null)
     {
+        if ($paths != null) {
+            $this->findPaths = $paths;
+        }
+
         if ($file != null) {
             $this->fileList = file($file);
         } else {
@@ -55,7 +60,7 @@ class Polaris_ClassMapGenerator
 
     /**
      * Generates the map and returns a serialized array map
-     * 
+     *
      * @return string
      */
     public function generateMap()
@@ -66,7 +71,7 @@ class Polaris_ClassMapGenerator
 
     /**
      * Iterats over the file list to populate them
-     * 
+     *
      * @return void
      */
     public function populateClassList()
@@ -78,9 +83,9 @@ class Polaris_ClassMapGenerator
 
     /**
      * Tokenizes a file and maps the classes and interfaces
-     * 
+     *
      * @param string $file The file to parse
-     * 
+     *
      * @return void
      */
     public function mapClasses($file)
